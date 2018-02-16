7ef3fe6fa27eedf85382670d48e1c36ed7aee001
Vinay <Vinay@Vinay>
2018-02-16 15:40:17 +0530

    my first commit

---
+++ src/NewTest.java
@@ -0,0 +1,68 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	 @Test (groups = { "Car" })
+	 
+	  public void Car1() {
+	 
+		  System.out.println("Batch Car - Test car 1");
+	 
+	  }
+	 
+	  @Test (groups = { "Car" })
+	 
+	  public void Car2() {
+	 
+		  System.out.println("Batch Car - Test car 2");
+	 
+	  }
+	 
+	  @Test (groups = { "Scooter" })
+	 
+	  public void Scooter1() {
+	 
+		  System.out.println("Batch Scooter - Test scooter 1");
+	 
+	  }
+	 
+	  @Test (groups = { "Scooter" })
+	 
+	  public void Scooter2() {
+	 
+		  System.out.println("Batch Scooter - Test scooter 2");
+	 
+	  }
+	 
+	  @Test (groups = { "Car", "Sedan Car" })
+	 
+	  public void Sedan1() {
+	 
+		  System.out.println("Batch Sedan Car - Test sedan 1");
+	 
+	  }
+	
+	
+	
+	
+  }
+ 
+  
+
+
